c8070f3fa2bbc6c67d576a62adb5755aed786ecd
stevekutz <stkutz@gmail.com>
2019-10-12 22:44:19 -0500

    added handler and spread style update

--- app/src/HeadelineHooks.js
+++ app/src/HeadelineHooks.js
@@ -7,11 +7,19 @@ const HeadlineHooks = () => {
     const [greeting, setGreetingInput] = useState('Default State Greeting')    
     const alignStyles = {width: '50%', margin: '5px auto', padding: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}                    
     const inputStyles = {border: '1px solid deeppink', color: 'dodgerblue', ...alignStyles}
-    const headerStyles = {fontWeight: 'heavy', color: 'darkred', ...alignStyles}
+    const headerStyles = {fontWeight: 'bold', color: 'darkred', ...alignStyles}
+    const inputStyles2 = {...inputStyles, ...alignStyles, ... {border: '4px solid seagreen'} }
+
+    const _handleInputs = (ev) => setGreetingInput(
+        ev.targetValue
+        
+        );
 
     return (
         <div >
             <Header style = {headerStyles}> {greeting}</Header>
+
+
 
             <Input
                 label="Input Text"
@@ -19,10 +27,17 @@ const HeadlineHooks = () => {
                 type="text"
                 style = {inputStyles}
                 value = {greeting}
-                onChange={ev => setGreetingInput(ev.taret.value)}
+                onChange={ev => setGreetingInput(ev.target.value)}
             />
         
-        
+            <Input
+            label="Input Text"
+            placeholder="Placeholder text"
+            type="text"
+            style = {inputStyles2}
+            value = {greeting}
+            onChange={ _handleInputs}
+            />
         </div>
     )
 

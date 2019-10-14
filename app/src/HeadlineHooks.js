9640da18d15e5533b93560f7411716b00c6521b5
stevekutz <stkutz@gmail.com>
2019-10-13 22:14:07 -0500

    applied useEffect to randomly change colors

--- app/src/HeadlineHooks.js
+++ app/src/HeadlineHooks.js
@@ -5,54 +5,59 @@ import {Input} from 'react-rainbow-components';
 const HeadlineHooks = () => {
 
     const [greeting, setGreetingInput] = useState('Default State Greeting')   
-    const [greeting2, setGreetingInput2] = useState('Default 2nd State Greeting') 
+    const [greeting2, setGreetingInput2] = useState('Default 2nd State Greeting')
     
+    const randomHsl = () => `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
+
     const alignStyles = {width: '50%', margin: '5px auto', padding: '5px', display: 'flex', flexDirection: 'column', alignItems: 'center'}                    
     const inputStyles = {border: '1px solid deeppink', color: 'dodgerblue', ...alignStyles}
     const headerStyles = {fontWeight: 'bold', color: 'darkred', ...alignStyles}
-    const inputStyles2 = {...inputStyles, ...alignStyles, ... {border: '4px solid seagreen'}, alignItems: 'stretch' }
-    const borderStyle = {border: '3px solid blue'}
+    const inputStyles2 = {...inputStyles, ...alignStyles, ...{border: '4px solid seagreen'}, alignItems: 'stretch' }
+    const borderStyle = {border: `10px solid ${randomHsl()} `}
 
     const _handleInputs = (ev) => setGreetingInput2(
         ev.target.value       
         );
 
+    useEffect( () => randomHsl , [greeting, greeting2] ); 
 
 
     return (
+        
         <div style = {borderStyle}>
-            <Header style = {headerStyles}> {greeting}</Header>
-            <Header style = {headerStyles}> {greeting2}</Header>
+                <p style = {{backgroundColor: `  ${randomHsl()}   `, textAlign: 'center'}}> useEffect does color change !</p>
 
-            <Input
-                label="ev passed inline to setGreetingInput"
-                placeholder="Placeholder text"
-                type="text"
-                style = {inputStyles}
-                value = {greeting}
-                onChange={ev => setGreetingInput(ev.target.value)}
-            />
+                <Header style = {headerStyles}> {greeting}</Header>
+                <Header style = {headerStyles}> {greeting2}</Header>
 
-            <Input
-                label="ev passed inline to setGreetingInput"
-                placeholder="Placeholder text"
-                type="text"
-                style = {inputStyles2}
-                size = 'mini'
-                value = {greeting2}
-                // onChange={(ev) => _handleInputs(ev)}  
-                onChange={ev => setGreetingInput2(ev.target.value)}
-            />
+                <Input
+                    label="ev passed inline to setGreetingInput"
+                    placeholder="Placeholder text"
+                    type="text"
+                    style = {inputStyles}
+                    value = {greeting}
+                    onChange={ev => setGreetingInput(ev.target.value)}
+                />
 
-            <Input
-                label="handler directly called"
-                placeholder="Placeholder text"
-                type="text"
-                style = {inputStyles2}
-                value = {greeting2}
-                onChange={_handleInputs}     
-            />
+                <Input
+                    label="ev passed inline to setGreetingInput"
+                    placeholder="Placeholder text"
+                    type="text"
+                    style = {inputStyles2}
+                    size = 'mini'
+                    value = {greeting2}
+                    // onChange={(ev) => _handleInputs(ev)}  
+                    onChange={ev => setGreetingInput2(ev.target.value)}
+                />
 
+                <Input
+                    label="handler directly called"
+                    placeholder="Placeholder text"
+                    type="text"
+                    style = {inputStyles2}
+                    value = {greeting2}
+                    onChange={_handleInputs}     
+                />
         </div>
     )
 
